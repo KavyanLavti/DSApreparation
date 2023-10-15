9d29c4c8fd824a21de2df6a9b2f1af481f69f68f
KavyanLavti <109773980+KavyanLavti@users.noreply.github.com>
2023-10-15 15:59:03 +0530

    Create ceil_inBST.java

---
+++ ceil_inBST.java
@@ -0,0 +1,44 @@
+import java.util.* ;
+import java.io.*; 
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    class TreeNode<T>
+    {
+       public:
+        T data;
+        TreeNode<T> left;
+        TreeNode<T> right;
+
+        TreeNode(T data)
+        {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    };
+
+************************************************************/
+
+public class Solution 
+{
+
+    public static int findCeil(TreeNode<Integer> node, int x) 
+    {
+        int suc = -1;
+        while(node!=null)
+        {
+            if(node.data<x)
+            {
+                node = node.right;
+            }
+            else
+            {
+                suc = node.data;
+                node = node.left;
+            }
+        }
+        return suc;
+    }
+}
